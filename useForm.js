89d57baab2d0b4de91ec41825f3c9aaf928bf2ed
camilobriceno <camilobriceno@hotmail.es>
2024-04-04 13:43:48 +0200

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+export const useForm = ( initialForm = {} ) => {
+
+
+  const [formState, setFormState] = useState( initialForm );
+
+//desesctructuro...
+
+const { username, email, password } = formState
+
+const onInputChange = ({target}) => {
+
+const { name, value } = target;
+
+  setFormState({
+
+    ...formState,
+    [ name ]: value
+  });
+  }
+
+
+  const onResetForm = () => {
+
+    setFormState( initialForm );
+
+  }
+
+  return {
+
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm,
+  }
+}
